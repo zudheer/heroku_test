525c2bf9fd11c8e042530494e7dc2c1142944ce4
sudheerkdty <sudheerkdty@gmail.com>
2018-06-26 19:24:50 +0530

    setup home page

--- terms/urls.py
+++ terms/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
